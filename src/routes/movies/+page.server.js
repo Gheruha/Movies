b37a10c7e73891ff320d5b2d94551c64c031b4f9
Gheruha <maxgheruha@gmail.com>
2024-05-31 15:57:51 +0300

    Regenerated key

--- src/routes/movies/+page.server.js
+++ src/routes/movies/+page.server.js
@@ -1,10 +1,8 @@
+import { key } from '$env/static/private';
 // @ts-ignore
-
 // Fetching popular movies with the api
-export async function load({ fetch, params }) {
-	const res = await fetch(
-		'https://api.themoviedb.org/3/discover/movie?api_key=dd0bd21ced2dd658d78f19de06d31ccf'
-	);
+export async function load({ fetch }) {
+	const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${key}`);
 
 	const data = await res.json();
 
--- src/routes/movies/[id]/+page.server.js
+++ src/routes/movies/[id]/+page.server.js
@@ -1,10 +1,8 @@
+import { key } from '$env/static/private';
 // @ts-ignore
-
 // Fetching images and videos with the api
 export async function load({ fetch, params }) {
-	const res = await fetch(
-		`https://api.themoviedb.org/3/movie/${params.id}?api_key=dd0bd21ced2dd658d78f19de06d31ccf`
-	);
+	const res = await fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=${key}`);
 	const video = await fetch(
 		`https://api.themoviedb.org/3/movie/${params.id}/videos?api_key=dd0bd21ced2dd658d78f19de06d31ccf`
 	);

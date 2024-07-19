376acb7a86078481a47afb270c5b877866660f95
Gheruha <maxgheruha@gmail.com>
2024-07-19 08:09:16 +0200

    Deploying v1

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,12 +1,6 @@
 export const prerender = true;
-import { VITE_KEY } from '$env/static/private';
 
-// @ts-ignore
-export async function load({ url, fetch }) {
-	const genres = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${VITE_KEY}`);
-	const genresDetail = await genres.json();
-
-	console.log(genresDetail);
+export async function load({ url }) {
 	return {
 		url: url.pathname
 	};

9dfadc2157c161cf05bb0074d579091435c990e6
Gheruha <maxgheruha@gmail.com>
2024-07-19 20:16:40 +0200

    Vercel v1.2

--- src/routes/movies/[id]/+page.server.js
+++ src/routes/movies/[id]/+page.server.js
@@ -1,31 +1,31 @@
-import { VITE_KEY } from '$env/static/private';
-// @ts-ignore
-// Fetching images and videos with the api
-export async function load({ fetch, params }) {
-	const res = await fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=${VITE_KEY}`);
-	const video = await fetch(
-		`https://api.themoviedb.org/3/movie/${params.id}/videos?api_key=${VITE_KEY}`
-	);
+// import { VITE_KEY } from '$env/static/private';
+// // @ts-ignore
+// // Fetching images and videos with the api
+// export async function load({ fetch, params }) {
+// 	const res = await fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=${VITE_KEY}`);
+// 	const video = await fetch(
+// 		`https://api.themoviedb.org/3/movie/${params.id}/videos?api_key=${VITE_KEY}`
+// 	);
 
-	const recommendations = await fetch(
-		`https://api.themoviedb.org/3/movie/${params.id}/recommendations?api_key=${VITE_KEY}`
-	);
+// 	const recommendations = await fetch(
+// 		`https://api.themoviedb.org/3/movie/${params.id}/recommendations?api_key=${VITE_KEY}`
+// 	);
 
-	const actors = await fetch(
-		`https://api.themoviedb.org/3/movie/${params.id}/credits?api_key=${VITE_KEY}`
-	);
+// 	const actors = await fetch(
+// 		`https://api.themoviedb.org/3/movie/${params.id}/credits?api_key=${VITE_KEY}`
+// 	);
 
-	const movieDetail = await res.json();
-	const videoDetail = await video.json();
-	const recommendationsDetail = await recommendations.json();
-	const actorsDetail = await actors.json();
+// 	const movieDetail = await res.json();
+// 	const videoDetail = await video.json();
+// 	const recommendationsDetail = await recommendations.json();
+// 	const actorsDetail = await actors.json();
 
-	if (res.ok) {
-		return {
-			data: movieDetail,
-			video: videoDetail,
-			recommendations: recommendationsDetail,
-			actors: actorsDetail
-		};
-	}
-}
+// 	if (res.ok) {
+// 		return {
+// 			data: movieDetail,
+// 			video: videoDetail,
+// 			recommendations: recommendationsDetail,
+// 			actors: actorsDetail
+// 		};
+// 	}
+// }

e4f93cf19d6c5f3b69a99c3ff561e5d88cdb86cf
Gheruha <maxgheruha@gmail.com>
2025-01-23 14:59:22 +0200

    Converted tailwind file in ES

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,28 @@
 /* tailwind.config.css */
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {
-			animation: {
-				blob: 'blob 7s infinite'
-			},
-			keyframes: {
-				blob: {
-					'0%': {
-						transform: 'translate(0px, 0px) scale(1)'
-					},
-					'33%': {
-						transform: 'translate(10px, -20px) scale(1.1)'
-					},
-					'66%': {
-						transform: 'translate(-10px, 5px) scale(0.9)'
-					},
-					'100%': {
-						transform: 'translate(0px, 0px) scale(1)'
-					}
+export const darkMode = 'class';
+export const content = ['./src/**/*.{html,js,svelte,ts}'];
+export const theme = {
+	extend: {
+		animation: {
+			blob: 'blob 7s infinite'
+		},
+		keyframes: {
+			blob: {
+				'0%': {
+					transform: 'translate(0px, 0px) scale(1)'
+				},
+				'33%': {
+					transform: 'translate(10px, -20px) scale(1.1)'
+				},
+				'66%': {
+					transform: 'translate(-10px, 5px) scale(0.9)'
+				},
+				'100%': {
+					transform: 'translate(0px, 0px) scale(1)'
 				}
 			}
 		}
-	},
-	plugins: []
+	}
 };
+export const plugins = [];
